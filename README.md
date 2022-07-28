c7672400a974d86e12c6e5aebac49eab125682f3
wesion <497905861@qq.com>
2022-07-28 15:09:08 +0800

    no message

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 <!--
  * @Author: wesion
  * @Date: 2022-07-22 17:08:03
- * @LastEditTime: 2022-07-25 16:50:51
+ * @LastEditTime: 2022-07-28 14:49:35
  * @Description: 
 -->
 # taroCreateTsx README
 
- taro 项目一键添加tsx和less文件
+ 项目一键添加模板文件文件
 
-## Features
-
+## 用法
+1. 安装插件
+2. 在项目**根目录**创建**template**文件夹
+3. 添加模板有2种
+* template里只有文件，无分级
+    >-template
+    >-template/a.js
+    >-template/b.tsx
+    >-template/...
+* template里有文件夹（<font color= red>当有文件夹存在，则只能通过选择对应文件夹下面的模板,template文件夹下的问价一概不会被创建</font>）
+    >-template
+    >-template/A
+    >-template/B
+    >-template/...
+4.  再template添加对应模板,如想替换对应模板文件下的关键key请用 **{componentName}** 
+5. 再对应想创建模板的文件下右键呼出界面
 
 **Enjoy!**
--- src/command/createTaroSpecificFile.ts
+++ src/command/createTaroSpecificFile.ts
@@ -1,57 +1,167 @@
 /*
  * @Author: wesion
  * @Date: 2022-07-25 10:21:38
- * @LastEditTime: 2022-07-25 16:44:03
+ * @LastEditTime: 2022-07-28 14:54:46
  * @Description: 
  */
 import * as vscode from 'vscode';
-const fse = require('fs-extra');
+import * as fse from 'fs-extra';
+const fs = require('fs');
+const rpath = require('path');
 
-const path = require('path');
+function showErrToast() {
+    vscode.window.showErrorMessage("添加失败");
+}
+
+/**
+ * 获取当前文件夹下的文件
+ * @param templateDirPath 模板路径(先去/template找，没有的话用tsx、less模板)
+ * @param targetDirPath 目标文件夹路径
+ */
+function getDirectoryContain(templateDirPath: string, targetDirPath: string) {
+    //判断当前是否有模板文件夹
+    vscode.workspace.fs.readDirectory(vscode.Uri.file(templateDirPath)).then(files => {
+        //template模板文件下对应的dir数和file数
+        var dirArr: string[] = [], fileArr: string[] = [];
+        files.map(e => {
+            if (e[1] === 2) {
+                dirArr.push(e[0]);
+            } else if (e[1] === 1) {
+                fileArr.push(e[0]);
+            }
+        });
+        //默认当template文件夹下有dir则弹出选择，2.当没有dir则去看对应file数，3.都没有的情况则去取默认文件夹
+        if (dirArr.length > 0) {
+            vscode.window.showQuickPick(dirArr,{placeHolder:"选择template文件创建的模板"}).then(e => {
+                let chgDirPath = templateDirPath + "/" + e;
+                getDirectoryContain(chgDirPath, targetDirPath);
+            });
+        } else if (fileArr.length > 0) {
+            vscode.window.showInputBox({ placeHolder: "输入文件名" }).then(name => {
+                if (!name) {
+                    return;
+                }
+                fileArr.map(templateFileName => {
+                    createComponentFile(templateDirPath, templateFileName, targetDirPath, name);
+                });
+            });
+
+        } else {
+            vscode.window.showErrorMessage("template文件目录下为空,请添加模板");
+        }
+
+
+
+
+
+    }, err => {
+        vscode.window.showInputBox({ placeHolder: "输入文件名" }).then(name => {
+            if (!name) {
+                return;
+            }
+            createDefaultComponentFile(targetDirPath, name);
+        });
+
+    });
+}
+//创建组件文件
+/**
+ * 
+* @param templateDirPath 模板文件夹路径
+ * @param templateFileName 模板文件名(全名带后缀)
+ * @param targetFilePath 目标文件路径
+ * @param targetFileName 目标文件名
+ */
+function createComponentFile(templateDirPath: string, templateFileName: string, targetFilePath: string, targetFileName: string) {
+
+    //拆分templateFileName，通过"."拆分
+    let templateFileNameArr = templateFileName.split('.');
+    //不带后缀的模板文件名
+    var templateFileNameWithoutSuffix = templateFileNameArr[0];
+    //模板文件名的后缀
+    var templateFileNameSuffix = "";
+    if (templateFileNameArr.length > 1) {
+        templateFileNameSuffix = templateFileName.substring(templateFileNameWithoutSuffix.length - 1);
+    }
+    //拼接好完整的目标路径
+    let targetPath = targetFilePath + '/' + targetFileName + templateFileNameSuffix;
+
+    fse.ensureFile(targetPath)
+        .then(() => {
+            //替换方法名->文件名
+            let componentContent = fse
+                .readFileSync(templateDirPath + "/" + templateFileName)
+                .toString()
+                .replace(/{componentName}/g, targetFileName);
+            fse.outputFile(targetPath, componentContent);
+
+            console.log('success!');
+        })
+        .catch((err: any) => {
+            console.error(err);
+        });
+}
+//
+/**
+ * 创建默认组件文件（tsx+less）
+ * @param targetPath 目标路径
+ * @param fileName 用户自主想创建的文件名
+ */
+function createDefaultComponentFile(targetDirPath: string, targetFileName: string) {
+    let tsxfile = targetDirPath + `/${targetFileName}.tsx`;
+    let lessfile = targetDirPath + `/${targetFileName}.module.less`;
+    //插件项目里的template路径
+    let templatesDir = rpath.join(__dirname);
+    fse.ensureFile(tsxfile)
+        .then(() => {
+
+            let componentContent = fse
+                .readFileSync(templatesDir + '/functional.template')
+                .toString().replace(/{componentName}/g, targetFileName);;
+            fse.outputFile(tsxfile, componentContent);
+            console.log('success!');
+        })
+        .catch((err: any) => {
+            showErrToast();
+        });
+    fse.ensureFile(lessfile)
+        .then(() => {
+
+            let componentContent = fse
+                .readFileSync(templatesDir + '/styles.template')
+                .toString();
+            fse.outputFile(lessfile, componentContent);
+            console.log('success!');
+        })
+        .catch((err: any) => {
+            showErrToast();
+        });
+}
+
 
 let createTaroSpecificFile = vscode.commands.registerCommand('extension.createTaroSpecificFile', (uri) => {
+
+
     if (!uri) {
         return vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
     }
-    vscode.window.showInputBox().then(e => {
-        let createUri: vscode.Uri = vscode.Uri.file(uri + '/1111.tsx');
-        console.log(uri + `${e}.tsx`);
+    var contextMenuSourcePath = uri.path;
+    //点击文件的时候获取当前文件的文件夹路径
+    if (uri && fs.lstatSync(uri.fsPath).isDirectory()) {
+        contextMenuSourcePath = uri.fsPath;
+    } else {
+        contextMenuSourcePath = rpath.dirname(uri.fsPath);
+    }
 
-        let tsxfile = uri.path + `/${e}.tsx`;
-        let lessfile = uri.path + `/${e}.module.less`;
+    // 获取当前项目的根目录
+    if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
+        //获取项目根目录
+        let rootPath = vscode.workspace.workspaceFolders[0].uri;
+        // //获取当前文件夹下的文件
+        getDirectoryContain(rootPath.path + '/template', contextMenuSourcePath);
+    }
 
-        let templatesDir = path.join(__dirname);
-       
 
-        fse.ensureFile(tsxfile)
-            .then(() => {
-                let templateFileName = templatesDir + '/functional.template';
-                //替换方法名->文件名
-                let componentContent = fse
-                .readFileSync(templateFileName)
-                .toString()
-                .replace(/{componentName}/g, e);
-                fse.outputFile(tsxfile, componentContent);
-
-                console.log('success!');
-            })
-            .catch((err: any) => {
-                console.error(err);
-            });
-        fse.ensureFile(lessfile)
-            .then(() => {
-
-                let componentContent = fse
-                .readFileSync( templatesDir + '/styles.template')
-                .toString();
-                fse.outputFile(lessfile, componentContent);
-                console.log('success!');
-            })
-            .catch((err: any) => {
-                console.error(err);
-            });
-
-    });
 
 
 
